b2b713bee83b3f734a5d2e88e2ef76f4c9164527
Bartek <bbogucki7@gmail.com>
2024-10-08 16:19:09 +0200

    created start component containers and started stylized them

--- src/components/StartComponent.tsx
+++ src/components/StartComponent.tsx
@@ -5,10 +5,22 @@ const StartComponent: React.FC = () => {
 		<div>
 			<div className='start-component'>
 				<div className='start-component__main-container'>
-					<div className='start-component__user-name-container'>
-						<div>Sign your name:</div>
+					<div className='start-component__container'>
+						<div className='start-component__user-name-container containers'>
+							<div>Sign your name:</div>
+							<input type='text' />
+						</div>
+						<div className='start-component__difficulty-level-container containers'>
+							<div>Select difficulty level:</div>
+							<button>Sports</button>
+							<button>Cars</button>
+							<button>Languages</button>
+						</div>
+						<div className='start-component__accept-button-container containers'>
+							<div>Press button if you wanna start the game.</div>
+							<button>Let's start the game!</button>
+						</div>
 					</div>
-					<div className='start-component__difficulty-level-container'></div>
 				</div>
 			</div>
 		</div>
