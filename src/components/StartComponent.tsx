5b6b10e1804448a220ce73f1ff243bdcc3fb13a0
Bartek <bbogucki7@gmail.com>
2024-10-08 20:04:13 +0200

    implemented Button Props to StartComponent

--- src/components/StartComponent.tsx
+++ src/components/StartComponent.tsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import "../sass/start-component.scss";
+import Button from "./shared-components/Button";
 
 const StartComponent: React.FC = () => {
+	const [isFocusDifficultyLevel, setIsFocusDifficultyLevel] = useState<string | null>(null);
+
 	return (
 		<div>
 			<div className='start-component'>
 				<div className='start-component__main-container'>
 					<div className='start-component__container'>
 						<div className='start-component__user-name-container containers'>
-							<div>Sign your name:</div>
-							<input type='text' />
+							<div className='start-component__short-description'>Sign your name:</div>
+							<input type='text' className='input' />
 						</div>
 						<div className='start-component__difficulty-level-container containers'>
-							<div>Select difficulty level:</div>
-							<button>Sports</button>
-							<button>Cars</button>
-							<button>Languages</button>
+							<div className='start-component__short-description'>Select difficulty level:</div>
+							<Button
+								focus={isFocusDifficultyLevel === "Sports"}
+								title='Sports'
+								message=' (easy)'
+								onClick={() => setIsFocusDifficultyLevel("Sports")}></Button>
+							<Button
+								focus={isFocusDifficultyLevel === "Cars"}
+								title='Cars'
+								message=' (medium)'
+								onClick={() => setIsFocusDifficultyLevel("Cars")}></Button>
+							<Button
+								focus={isFocusDifficultyLevel === "Languages"}
+								title='Languages'
+								message=' (hard)'
+								onClick={() => setIsFocusDifficultyLevel("Languages")}></Button>
 						</div>
 						<div className='start-component__accept-button-container containers'>
-							<div>Press button if you wanna start the game.</div>
-							<button>Let's start the game!</button>
+							<div className='start-component__short-description'>Press button to start the game.</div>
+							<Button title="Let's start the game!" ></Button>
 						</div>
 					</div>
 				</div>
--- src/components/shared-components/Button.tsx
+++ src/components/shared-components/Button.tsx
@@ -1,6 +1,18 @@
+import "../../sass/shared-styles/button.scss";
 
+interface ButtonProps {
+	focus?: boolean;
+	title: string;
+	message?: string;
+	onClick?: () => void;
+}
 
-const Button: React.FC = () => {
-	return <div></div>;
+const Button: React.FC<ButtonProps> = ({ focus, title, message, onClick }: ButtonProps) => {
+	return (
+		<button className='button' data-focus={focus} onClick={onClick}>
+			{title}
+			{message}
+		</button>
+	);
 };
 export default Button;
