81759effd55b246b75bf3ab064c231b0ba1be1c3
Bartek <bbogucki7@gmail.com>
2024-10-11 10:55:18 +0200

    implemented conditions about flippedCards in Card component

--- src/components/shared-components/Card.tsx
+++ src/components/shared-components/Card.tsx
@@ -5,11 +5,26 @@ import useUserCurrentDataState from "../../store/userCurrentDataStore";
 const Card: React.FC = () => {
 	const { photosToFields } = useUserCurrentDataState();
 	const [isCardRotated, setIsCardRotated] = useState<boolean[]>(Array(photosToFields.length).fill(false));
+	const [flippedCards, setFlippedCards] = useState<number[]>([]);
 
 	const handleRotateParticularCard = (index: number) => {
 		const spreadWholeRotatedCards = [...isCardRotated];
 		spreadWholeRotatedCards[index] = !spreadWholeRotatedCards[index];
 		setIsCardRotated(spreadWholeRotatedCards);
+
+		if (spreadWholeRotatedCards[index]) {
+			flippedCards.push(index);
+			console.log(flippedCards);
+		}
+		if (flippedCards.length === 2) {
+			setFlippedCards([]);
+			setTimeout(() => {
+				setIsCardRotated(Array(photosToFields.length).fill(false));
+			}, 300);
+		}
+		if (flippedCards[0] === flippedCards[1]) {
+			console.log("kokosik");
+		}
 	};
 
 	useEffect(() => {
